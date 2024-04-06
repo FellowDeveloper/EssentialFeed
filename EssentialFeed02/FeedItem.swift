b0bf7e2b1a4cdd424492a4b8ef946984731342b3
Anton Tugolukov <anton.tugolukov@gmail.com>
2024-04-05 21:10:27 -0700

    Initial Commit

---
+++ EssentialFeed02/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed02
+//
+//  Created by Anton Tugolukov on 4/5/24.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let descripion: String?
+    let location: String?
+    let imageURL: URL
+}
