9f440cc0250dc720ef6b3d6d37e99279e4596485
Frustrated Developer <anton.tugolukov@gmail.com>
2025-02-09 15:42:25 -0800

    Avoid sharing state in E2E tests by using ephemeral session configuration (no url cache)

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -28,7 +28,7 @@ final class EssentialFeedAPIEndToEndTests: XCTestCase {
     // MARK: - Helpers
     private func getFeedResult(file: StaticString = #file, line: UInt = #line) -> LoadFeedResult? {
         let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
-        let client = URLSessionHTTPClient()
+        let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
         trackForMemoryLeaks(client, file: file, line: line)
         let loader = RemoteFeedLoader(url: testServerURL, client: client)
         trackForMemoryLeaks(loader, file: file, line: line)
